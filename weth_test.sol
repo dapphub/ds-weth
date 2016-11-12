89eefe7f3f7db683a51967deeaddb8a36880bfc6
Daniel Brockman <daniel@brockman.se>
2016-11-12 15:38:07 +0100

    add much more comprehensive tests

--- weth_test.sol
+++ weth_test.sol
@@ -3,50 +3,208 @@ pragma solidity ^0.4.4;
 import "dapple/test.sol";
 import "weth.sol";
 
-contract WETHTest is Test {
-    event Deposit    (address indexed owner, uint value);
-    event Withdrawal (address indexed owner, uint value);
-
-    WETH weth = new WETH();
-
+contract Person {
+    WETH weth;
+    function Person(WETH _weth) { weth = _weth; }
     function () payable {}
+    function deposit() payable { weth.deposit.value(msg.value)(); }
+    function withdraw(uint value) { weth.withdraw(value); }
+    function transfer(Person recipient, uint value) {
+        if (!weth.transfer(recipient, value)) throw;
+    }
+    function approve(Person spender, uint value) {
+        if (!weth.approve(spender, value)) throw;
+    }
+    function transfer(Person owner, Person recipient, uint value) {
+        if (!weth.transferFrom(owner, recipient, value)) throw;
+    }
+}
 
-    function test_initial() {
-        assertEq(weth.totalSupply(), 0);
-        assertEq(weth.balanceOf(this), 0);
+contract WETHTest is Test {
+    WETH    weth   = new WETH();
+    Person  alice  = new Person(weth);
+    Person  bob    = new Person(weth);
+    Person  carol  = new Person(weth);
+
+    function test_initial_state() {
+        assert_eth_balance   (alice, 0 finney);
+        assert_weth_balance  (alice, 0 finney);
+        assert_eth_balance   (bob,   0 finney);
+        assert_weth_balance  (bob,   0 finney);
+        assert_eth_balance   (carol, 0 finney);
+        assert_weth_balance  (carol, 0 finney);
+
+        assert_weth_supply   (0 finney);
     }
 
     function test_deposit() {
-        expectEventsExact(weth);
+        expectEventsExact    (weth);
 
-        Deposit(this, 3);
-        weth.deposit.value(3)();
-        assertEq(weth.balanceOf(this), 3);
-        assertEq(weth.totalSupply(), 3);
+        perform_deposit      (alice,  3 finney);
+        assert_weth_balance  (alice,  3 finney);
+        assert_weth_balance  (bob,    0 finney);
+        assert_eth_balance   (alice,  0 finney);
+        assert_weth_supply   (3 finney);
 
-        Deposit(this, 4);
-        weth.deposit.value(4)();
-        assertEq(weth.balanceOf(this), 7);
-        assertEq(weth.totalSupply(), 7);
+        perform_deposit      (alice,  4 finney);
+        assert_weth_balance  (alice,  7 finney);
+        assert_weth_balance  (bob,    0 finney);
+        assert_eth_balance   (alice,  0 finney);
+        assert_weth_supply   (7 finney);
+
+        perform_deposit      (bob,    5 finney);
+        assert_weth_balance  (bob,    5 finney);
+        assert_weth_balance  (alice,  7 finney);
+        assert_weth_supply   (12 finney);
     }
 
-    function testFail_withdraw() {
-        weth.withdraw(1);
+    function testFail_withdrawal_1() {
+        perform_withdrawal   (alice,  1 wei);
     }
 
-    function test_withdraw() {
-        uint original = this.balance;
-        expectEventsExact(weth);
+    function testFail_withdrawal_2() {
+        perform_deposit      (alice,  1 finney);
+        perform_withdrawal   (bob,    1 wei);
+    }
 
-        Deposit(this, 7);
-        weth.deposit.value(7)();
+    function testFail_withdrawal_3() {
+        perform_deposit      (alice,  1 finney);
+        perform_deposit      (bob,    1 finney);
+        perform_withdrawal   (bob,    1 finney);
+        perform_withdrawal   (bob,    1 wei);
+    }
 
-        Withdrawal(this, 3);
-        weth.withdraw(3);
-        assertEq(this.balance, original - 4);
+    function test_withdrawal() {
+        expectEventsExact    (weth);
 
-        Withdrawal(this, 4);
-        weth.withdraw(4);
-        assertEq(this.balance, original);
+        perform_deposit      (alice,  7 finney);
+        assert_weth_balance  (alice,  7 finney);
+        assert_eth_balance   (alice,  0 finney);
+
+        perform_withdrawal   (alice,  3 finney);
+        assert_weth_balance  (alice,  4 finney);
+        assert_eth_balance   (alice,  3 finney);
+
+        perform_withdrawal   (alice,  4 finney);
+        assert_weth_balance  (alice,  0 finney);
+        assert_eth_balance   (alice,  7 finney);
+    }
+
+    function testFail_transfer_1() {
+        perform_transfer     (alice,  1 wei, bob);
+    }
+
+    function testFail_transfer_2() {
+        perform_deposit      (alice,  1 finney);
+        perform_withdrawal   (alice,  1 finney);
+        perform_transfer     (alice,  1 wei, bob);
+    }
+
+    function test_transfer() {
+        expectEventsExact    (weth);
+
+        perform_deposit      (alice,  7 finney);
+        perform_transfer     (alice,  3 finney, bob);
+        assert_weth_balance  (alice,  4 finney);
+        assert_weth_balance  (bob,    3 finney);
+        assert_weth_supply   (7 finney);
+    }
+
+    function testFail_transferFrom_1() {
+        perform_transfer     (alice,  1 wei, bob, carol);
+    }
+
+    function testFail_transferFrom_2() {
+        perform_deposit      (alice,  7 finney);
+        perform_approval     (alice,  3 finney, bob);
+        perform_transfer     (bob,    4 finney, alice, carol);
+    }
+
+    function test_transferFrom() {
+        expectEventsExact    (weth);
+
+        perform_deposit      (alice,  7 finney);
+        perform_approval     (alice,  5 finney, bob);
+        assert_weth_balance  (alice,  7 finney);
+        assert_allowance     (bob,    5 finney, alice);
+        assert_weth_supply   (7 finney);
+
+        perform_transfer     (bob,    3 finney, alice, carol);
+        assert_weth_balance  (alice,  4 finney);
+        assert_weth_balance  (bob,    0 finney);
+        assert_weth_balance  (carol,  3 finney);
+        assert_allowance     (bob,    2 finney, alice);
+        assert_weth_supply   (7 finney);
+
+        perform_transfer     (bob,    2 finney, alice, carol);
+        assert_weth_balance  (alice,  2 finney);
+        assert_weth_balance  (bob,    0 finney);
+        assert_weth_balance  (carol,  5 finney);
+        assert_allowance     (bob,    0 finney, alice);
+        assert_weth_supply   (7 finney);
+    }
+
+    //------------------------------------------------------------------
+    // Helper functions
+    //------------------------------------------------------------------
+
+    function assert_eth_balance(Person person, uint balance) {
+        assertEq(person.balance, balance);
+    }
+
+    function assert_weth_balance(Person person, uint balance) {
+        assertEq(weth.balanceOf(person), balance);
+    }
+
+    function assert_weth_supply(uint supply) {
+        assertEq(weth.totalSupply(), supply);
+    }
+
+    event Deposit     (address indexed owner, uint value);
+    event Withdrawal  (address indexed owner, uint value);
+
+    function perform_deposit(Person owner, uint value) {
+        Deposit(owner, value);
+        owner.deposit.value(value)();
+    }
+
+    function perform_withdrawal(Person owner, uint value) {
+        Withdrawal(owner, value);
+        owner.withdraw(value);
+    }
+
+    event Transfer(
+        address  indexed  owner,
+        address  indexed  recipient,
+        uint              value
+    );
+
+    function perform_transfer(
+        Person owner, uint value, Person recipient
+    ) {
+        Transfer(owner, recipient, value);
+        owner.transfer(recipient, value);
+    }
+
+    event Approval(
+        address  indexed  owner,
+        address  indexed  spender,
+        uint              value
+    );
+
+    function perform_approval(Person owner, uint value, Person spender) {
+        Approval(owner, spender, value);
+        owner.approve(spender, value);
+    }
+
+    function assert_allowance(Person spender, uint value, Person owner) {
+        assertEq(weth.allowance(owner, spender), value);
+    }
+
+    function perform_transfer(
+        Person spender, uint value, Person owner, Person recipient
+    ) {
+        Transfer(owner, recipient, value);
+        spender.transfer(owner, recipient, value);
     }
 }
